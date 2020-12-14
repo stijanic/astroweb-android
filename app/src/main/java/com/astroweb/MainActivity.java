d2f67727329a98666e43595981ef42a30d445052
stijanic <stijanic@yahoo.fr>
2020-12-14 02:29:04 +0100

    Monkey testing

--- app/src/main/java/com/astroweb/MainActivity.java
+++ app/src/main/java/com/astroweb/MainActivity.java
@@ -4,7 +4,9 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -62,6 +64,17 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        // MotionEvent object holds X-Y values
+        if(event.getAction() == MotionEvent.ACTION_DOWN) {
+            String text = "You click at x = " + event.getX() + " and y = " + event.getY();
+            Toast.makeText(this, text, Toast.LENGTH_LONG).show();
+        }
+
+        return super.onTouchEvent(event);
     }
 
     public void callAstrowebServer() {
---
+++ monkey.py
@@ -0,0 +1,39 @@
+# export PATH=/usr/lib/jvm/java-8-openjdk-amd64/bin:$PATH
+# /home/stijanic/Android/Sdk/tools/bin/monkeyrunner ~/monkey.py
+# Imports the monkeyrunner modules used by this program
+from com.android.monkeyrunner import MonkeyRunner, MonkeyDevice
+
+# Connects to the current device, returning a MonkeyDevice object
+device = MonkeyRunner.waitForConnection()
+
+# Installs the Android package. Notice that this method returns a boolean, so you can test
+# to see if the installation worked.
+# device.removePackage('com.astroweb')
+# MonkeyRunner.sleep (30)
+# device.installPackage('./app/build/outputs/apk/debug/app-debug.apk')
+
+# sets a variable with the package's internal name
+package = 'com.astroweb'
+
+# sets a variable with the name of an Activity in the package
+activity = 'com.astroweb.MainActivity'
+
+# sets the name of the component to start
+runComponent = package + '/' + activity
+
+# Runs the component
+device.startActivity(component=runComponent)
+
+# Presses the Menu button
+# device.press('KEYCODE_MENU', MonkeyDevice.DOWN_AND_UP)
+i = 1
+while i < 6:
+    MonkeyRunner.sleep (5)
+    device.touch(680, 1050, 'DOWN_AND_UP')
+    i += 1
+
+# Takes a screenshot
+# result = device.takeSnapshot()
+
+# Writes the screenshot to a file
+# result.writeToFile('shot1.png','png')
